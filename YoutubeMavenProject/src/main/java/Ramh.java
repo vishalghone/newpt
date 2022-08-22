bd0645d57ae1b5527a5becd190e3a988f6b82cc3
vishalghone <107985715+vishalghone@users.noreply.github.com>
2022-08-22 15:18:52 +0530

    Update Ramh.java

--- YoutubeMavenProject/src/main/java/Ramh.java
+++ YoutubeMavenProject/src/main/java/Ramh.java
@@ -3,7 +3,7 @@ public class Ramh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello world 2");
 
 	}
 
